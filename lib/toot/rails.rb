46111ede83016c4c05748627c6bc5e9bf2c1c0f0
Travis Petticrew <travis@petticrew.net>
2015-10-22 16:36:15 -0500

    Fix Railtie
    I was using the rake task handler incorrectly. This fixes it.

--- lib/toot/rails.rb
+++ lib/toot/rails.rb
@@ -1,7 +1,9 @@
 module Toot
   class Rails < Rails::Railtie
     rake_tasks do
-      Dir.glob(File.expand_path("../../tasks/*.rake", __FILE__))
+      Dir.glob(File.expand_path("../../tasks/*.rake", __FILE__)).each do |rake_file|
+        load rake_file
+      end
     end
   end if defined?(Rails::Railtie)
 end
