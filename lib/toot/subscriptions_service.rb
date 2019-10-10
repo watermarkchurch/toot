0d8f77e7d8b2d74c018ddf76744fdd664d808735
Gordon <gburgett@watermark.org>
2019-10-10 16:04:54 -0500

    Return subscription info in json from sub service

--- lib/toot/subscriptions_service.rb
+++ lib/toot/subscriptions_service.rb
@@ -1,9 +1,43 @@
 module Toot
   class SubscriptionsService
 
-    def call(env)
+    def self.call(env)
       request = Rack::Request.new(env)
       response = Rack::Response.new
+      me = new(request, response)
+
+      if me.respond_to?(request.request_method.downcase)
+        me.public_send(request.request_method.downcase)
+      else
+        # Method not allowed
+        response.status = 405
+      end
+
+      response.finish
+    end
+
+    attr_reader :request, :response
+    def initialize(request, response)
+      @request = request
+      @response = response
+    end
+
+    def get
+      accept = request.get_header('HTTP_ACCEPT')
+      
+      if accept.include?('application/json')
+        response.add_header('Content-Type', 'application/json')
+        response.write({
+          channels: channels,
+          subscriptions: subscriptions,
+        }.to_json)
+        return
+      end
+
+      response.status = 406
+    end
+
+    def post
       json = parse_body_json(request)
 
       if json["channel"] && json["callback_url"]
@@ -13,19 +47,24 @@ module Toot
       else
         response.status = 422
       end
-
-      response.finish
     end
 
-    def self.call(env)
-      new.call(env)
-    end
-
-    private def parse_body_json(request)
+    private
+    
+    def parse_body_json(request)
       JSON.parse(request.body.read)
     rescue JSON::ParserError
       {}
     end
 
+    def channels
+      @channels ||= Toot.redis { |r| r.keys(Toot.config.channel_prefix + "*") }
+    end
+
+    def subscriptions
+      @subscriptions ||= channels.each_with_object({}) do |ch, h|
+        h[ch] = Toot.redis { |r| r.smembers ch }
+      end
+    end
   end
 end
--- spec/toot/subscriptions_service_spec.rb
+++ spec/toot/subscriptions_service_spec.rb
@@ -4,36 +4,80 @@ require 'spec_helper'
 
 RSpec.describe Toot::SubscriptionsService do
   let(:connection) { instance_spy(Redis) }
-  let(:env) {
-    {
-      'REQUEST_METHOD' => 'POST',
-      'rack.input' => StringIO.new('{"callback_url":"http://example.com/callback","channel":"test.channel"}')
-    }
-  }
 
   before do
     allow(Toot).to receive(:redis) do |&blk|
       blk.call(connection)
     end
+
+    Toot.config.channel_prefix = 'wcc.toot.test.'
   end
 
-  it "adds the given callback_url to the channel's set in Redis" do
-    response = Rack::MockResponse.new(*described_class.call(env))
-    expect(connection).to have_received(:sadd).with('test.channel', 'http://example.com/callback')
-    expect(response.status).to eq(200)
+  describe 'post' do
+    let(:env) {
+      {
+        'REQUEST_METHOD' => 'POST',
+        'rack.input' => StringIO.new('{"callback_url":"http://example.com/callback","channel":"test.channel"}')
+      }
+    }
+
+    it "adds the given callback_url to the channel's set in Redis" do
+      # act
+      response = Rack::MockResponse.new(*described_class.call(env))
+
+      expect(connection).to have_received(:sadd).with('test.channel', 'http://example.com/callback')
+      expect(response.status).to eq(200)
+    end
+
+    it "does nothing and return 422 if channel isn't set" do
+      env['rack.input'] = StringIO.new('{"callback_url":"http://example.com/callback"}')
+
+      # act
+      response = Rack::MockResponse.new(*described_class.call(env))
+
+      expect(connection).to_not have_received(:sadd)
+      expect(response.status).to eq(422)
+    end
+
+    it "does nothing and returns 422 if callback_url isn't set" do
+      env['rack.input'] = StringIO.new('{"channel":"test.channel"}')
+
+      # act
+      response = Rack::MockResponse.new(*described_class.call(env))
+
+      expect(connection).to_not have_received(:sadd)
+      expect(response.status).to eq(422)
+    end
   end
 
-  it "does nothing and return 422 if channel isn't set" do
-    env['rack.input'] = StringIO.new('{"callback_url":"http://example.com/callback"}')
-    response = Rack::MockResponse.new(*described_class.call(env))
-    expect(connection).to_not have_received(:sadd)
-    expect(response.status).to eq(422)
-  end
+  describe 'get json' do
+    let(:env) {
+      {
+        'REQUEST_METHOD' => 'GET',
+        'HTTP_ACCEPT' => 'application/json'
+      }
+    }
 
-  it "does nothing and returns 422 if callback_url isn't set" do
-    env['rack.input'] = StringIO.new('{"channel":"test.channel"}')
-    response = Rack::MockResponse.new(*described_class.call(env))
-    expect(connection).to_not have_received(:sadd)
-    expect(response.status).to eq(422)
+    it 'lists channels and subscriptions' do
+      allow(connection).to receive(:keys)
+        .and_return(['wcc.toot.test.x', 'wcc.toot.test.y'])
+
+      allow(connection).to receive(:smembers)
+        .with('wcc.toot.test.x')
+        .and_return(['https://test.com/webhook'])
+      allow(connection).to receive(:smembers)
+        .with('wcc.toot.test.y')
+        .and_return(['https://test2.com/webhook'])
+
+      # act
+      response = Rack::MockResponse.new(*described_class.call(env))
+
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq('application/json')
+      body = JSON.parse(response.body)
+      expect(body['channels']).to eq(['wcc.toot.test.x', 'wcc.toot.test.y'])
+      expect(body['subscriptions']['wcc.toot.test.x'])
+        .to eq(['https://test.com/webhook'])
+    end
   end
 end
